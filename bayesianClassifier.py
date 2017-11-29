3688d0ce1ba8b6218421aef9f706ffdf384eac0a
Akshay Trikha <akshaytrikha@gmail.com>
2017-11-28 21:07:55 -0800

    Add files via upload
    added functions for making sentence length dictionary, obtaining words, making word length dictionary, making word count dictionary, obtaining punctuation, making punctuation count dictionary

--- bayesianClassifier.py
+++ bayesianClassifier.py
@@ -6,14 +6,19 @@
 
 from collections import defaultdict
 
+# TODO: fix getWords to include case for non-sentence ending punctuation with two spaces in between it
+
 input1 = ""
 input2 = ""
 
 endPunc = [".", "!", "?"] # sentence ending punctuation
-nonEndPunc = [";", ":", ",", "-", "&", "–", "(", ")", "{", "}", "[", "]", "<", ">", '"', "'", "`", ] # non-sentence ending punctuation
-# consider the newline 
+nonEndPunc = [";", ":", ",", "-", "&", "–", "(", ")", "{", "}", "[", "]", "<", ">", '"', "'", "`", "”", "“"] # non-sentence ending punctuation
+# different quotation mark types are included in list
+# TODO: consider slanted single quotes
+# TODO: consider the newline 
 
-def readText(fileName1):
+
+def readText (fileName1):
     # reads text from a textFile
     currentFile = open(fileName1, "r")
     text = currentFile.readlines()
@@ -29,11 +34,10 @@ def readText(fileName1):
 
     return wordList # main list for algorithm
 
-def getSentences(wordList):
+def getSentences (wordList):
     #  returns a list containing sentences
     if (wordList == []):
         return []
-    # elif (wordList[-1][-1] not in endPunc):
     else:
         sentenceList = []
         for i in range(len(wordList)):
@@ -41,22 +45,133 @@ def getSentences(wordList):
                 sentenceList.append(wordList[:i + 1])
                 print(sentenceList)
                 return sentenceList + getSentences(wordList[i + 1:])
-
-    # sentenceList = []
-    # # wordListCopy = wordList
-
-    # for i in range(len(wordList)):
-    #     if wordList[i][-1] in endPunc:
-    #         sentenceList.append(wordList[:i])
-    #         wordList = wordList[i:]
-
-    # print()
-    # print(sentenceList)
     
     return sentenceList
 
-dick = readText("testText.txt")
-getSentences(dick)
+def makeSentenceDictionary (sentenceList):
+    # returns a dictionary of sentences lengths and their frequencies
+    sentenceDictionary = {}
+    for i in sentenceList:
+        if not (len(i) in list(sentenceDictionary.keys())):
+            sentenceDictionary[len(i)] = 1
+        else:
+            sentenceDictionary[len(i)] += 1
+    print("sentence dictionary")
+    print(sentenceDictionary)
+    print("")
+    
+    return sentenceDictionary
+
+def getWords (wordList):
+    # returns a list containing words (with no punctuation)
+    for i in range(len(wordList)): # to clean list of all puntuation
+        if wordList[i] in nonEndPunc or wordList[i] in endPunc:
+            del wordList[i]
+        elif wordList[i][-1] in nonEndPunc or wordList[i][-1] in endPunc:
+            if len(wordList[i]) >= 2: # to avoid a index out of bounds error
+                if wordList[i][-2] in nonEndPunc or wordList[i][-2] in endPunc: 
+                    wordList[i] = wordList[i][:-2]
+                else:
+                    wordList[i] = wordList[i][:-1]
+            else:
+                wordList[i] = wordList[i][:-1]
+        elif wordList[i][0] in nonEndPunc or wordList[i][0] in endPunc:
+            wordList[i] = wordList[i][1:]
+
+    print("word list")
+    print(wordList)
+    print("")
+
+    return wordList
+
+def makeWordLengthDictionary (wordList):
+    # returns a dictionary of word lengths and their frequencies
+    wordLengthDictionary = {}
+    for i in wordList:
+        if not (len(i) in list(wordLengthDictionary.keys())):
+            wordLengthDictionary[len(i)] = 1
+        else:
+            wordLengthDictionary[len(i)] += 1
+
+    print("word length dict")
+    print(wordLengthDictionary)
+    print("")
+    
+    return wordLengthDictionary
+
+def makeWordCountDictionary (wordList):
+    # returns a dictionary of words and their frequencies
+    wordCountDictionary = {}
+    for i in wordList:
+        if not (i in list(wordCountDictionary.keys())):
+            wordCountDictionary[i] = 1
+        else:
+            wordCountDictionary[i] += 1
+
+    print("word count dict")
+    print(wordCountDictionary)
+    print("")
+    
+    return wordCountDictionary
+
+def getPunc (wordList):
+    # returns a list containing 
+    puncList = []
+    for i in range(len(wordList)): # to clean list of all puntuation
+        if wordList[i][-1] in nonEndPunc or wordList[i][-1] in endPunc:
+            puncList.append(wordList[i][-1])
+
+            if len(wordList[i]) >= 2: # to avoid a index out of bounds error
+                if wordList[i][-2] in nonEndPunc or wordList[i][-2] in endPunc: 
+                    puncList.append(wordList[i][-2])
+
+        if wordList[i][0] in nonEndPunc or wordList[i][0] in endPunc:
+            puncList.append(wordList[i][0])
+
+    print("punctuation list")
+    print(puncList)
+    print("")
+
+    return puncList
+
+def makePuncCountDictionary (puncList):
+    # returns a dictionary of punctuation marks and their frequencies
+    puncCountDictionary = {}
+    for i in puncList:
+        if not (i in list(puncCountDictionary.keys())):
+            puncCountDictionary[i] = 1
+        else:
+            puncCountDictionary[i] += 1
+
+    print("punctuation count dict")
+    print(puncCountDictionary)
+    print("")
+    
+    return puncCountDictionary
+
+
+
+
+
+
+
+
+
+text1 = readText("testText.txt") # a wordList
+sentencesText1 = getSentences(text1) # a sentenceList
+puncs = getPunc(text1) # a puncList
+
+
+justWords = getWords(text1) # a list of only words (no punctuation)
+print("juuuuuuust words",justWords)
+makeWordLengthDictionary(justWords) # dictionary of key = length of word, value = frequency
+makeWordCountDictionary(justWords) # dictionary of key = word, value = frequency
+makePuncCountDictionary(puncs) # dictionary of key = punctuation, value = frequency
+
+makeSentenceDictionary(sentencesText1) # dictionary of key = length of sentences, value = frequency
+
+
+
 
 
 TextModel1 = [ ]  # start with the empty list
