5cc8677c8e6718865f71f8011b15155ffce987ae
Akshay Trikha <akshaytrikha@gmail.com>
2017-12-08 15:47:01 -0800

    functional version of algorithm

--- textID.py
+++ textID.py
@@ -1,28 +1,12 @@
-# textmodel.py
+# TextID project!
 #
-# TextModel project!
-#
-# names: A-Dawg, V-Bank, E-Money
+# names : A-Dawg, V-Bank, E-Money
+# AKA   : Akshay Trikha, Violet Burbank, Elena Ehrlich
 
 from collections import defaultdict
 import math
 
-"""
-    Milestone methods:
-
-    readTextFromFile(self, filename): should take in a filename (a string) and should return all of the text in that file as a single, very large string
-    makeSentenceLengths(self, s): should use the text in the input string s to create the self.sentencelengths dictionary
-    printAllDictionaries(self): should print out all five of self's dictionaries in full - for testing and checking how they are working...
-    cleanString(self,s): should take in a string s and return a string with no punctuation and no upper-case letters
-    makeWordLengths(self, s): should use the text in the input string s to create the self.wordlengths dictionary
-    makeWords(self, s): should use the text in the input string s to create the self.words dictionary
-    makeStems(self, s): should use the text in the input string s to create the self.stems dictionary
-"""
-
 # TODO: fix getWords to include case for non-sentence ending punctuation with two spaces in between it
-
-input1 = ""
-input2 = ""
 
 endPunc = [".", "!", "?"] # sentence ending punctuation
 nonEndPunc = [";", ":", ",", "-", "&", "–", "(", ")", "{", "}", "[", "]", "<", ">", '"', "'", "`", "”", "“"] # non-sentence ending punctuation
@@ -35,7 +19,7 @@ suffixCons = ["s"]
 suffixVow2Cons = ["ing"]
 
 def readTextFromFile (fileName1):
-    # reads text from a textFile
+    """ returns a wordList after reading text from a textFile """
     currentFile = open(fileName1, "r")
     text = currentFile.readlines()
     currentFile.close()
@@ -49,7 +33,7 @@ def readTextFromFile (fileName1):
     return wordList # main list for algorithm
 
 def getSentences (wordList):
-    #  returns a list containing sentences
+    """ returns a list containing sentences """
     if (wordList == []):
         return []
     else:
@@ -60,12 +44,8 @@ def getSentences (wordList):
                 return sentenceList + getSentences(wordList[i + 1:])
     return sentenceList
 
-# wordList = ["I", "am", "a", "penis.", "Violet", "is", "also", "a", "penis."]
-
-# print(getSentences(wordList))
-
 def makeSentenceLengths (sentenceList):
-    # returns a dictionary of sentences lengths and their frequencies
+    """ returns a dictionary of sentences lengths and their frequencies """
     sentenceDictionary = {}
     for i in sentenceList:
         if not (len(i) in list(sentenceDictionary.keys())):
@@ -75,27 +55,28 @@ def makeSentenceLengths (sentenceList):
     
     return sentenceDictionary
 
-# TODO: modify this getWords so that it doesn't mutate text
 def getWords (wordList):
-    # returns a list containing words (with no punctuation)
+    """ returns a list containing words (with no punctuation) """
+    outputList = []
+
     for i in range(len(wordList)): # to clean list of all puntuation
         if wordList[i] in nonEndPunc or wordList[i] in endPunc: # to clean word if it is just punctuation
-            del wordList[i]
+            pass
         elif wordList[i][-1] in nonEndPunc or wordList[i][-1] in endPunc: # to clean end of word punctuation
             if len(wordList[i]) >= 2: # to avoid a index out of bounds error
                 if wordList[i][-2] in nonEndPunc or wordList[i][-2] in endPunc: # to clean 2nd from last punctuation
-                    wordList[i] = wordList[i][:-2]
+                    outputList.append(wordList[i][:-2])
                 else:
-                    wordList[i] = wordList[i][:-1]
+                    outputList.append(wordList[i][:-1])
             else:
-                wordList[i] = wordList[i][:-1]
+                outputList.append(wordList[i][:-1])
         elif wordList[i][0] in nonEndPunc or wordList[i][0] in endPunc:
-            wordList[i] = wordList[i][1:]
+            outputList.append(wordList[i][1:])
 
-    return wordList
+    return outputList
 
 def makeWordLengthDictionary (wordList):
-    # returns a dictionary of word lengths and their frequencies
+    """ returns a dictionary of word lengths and their frequencies """
     wordLengthDictionary = {}
     for i in wordList:
         if not (len(i) in list(wordLengthDictionary.keys())):
@@ -106,7 +87,7 @@ def makeWordLengthDictionary (wordList):
     return wordLengthDictionary
 
 def makeWordCountDictionary (wordList):
-    # returns a dictionary of words and their frequencies
+    """ returns a dictionary of words and their frequencies """
     wordCountDictionary = {}
     for i in wordList:
         if not (i in list(wordCountDictionary.keys())):
@@ -117,7 +98,7 @@ def makeWordCountDictionary (wordList):
     return wordCountDictionary
 
 def getPunc (wordList):
-    # returns a list containing all forms of punctuation found
+    """ returns a list containing all forms of punctuation found """
     puncList = []
     for i in range(len(wordList)): # to clean list of all puntuation
         if wordList[i][-1] in nonEndPunc or wordList[i][-1] in endPunc:
@@ -133,7 +114,7 @@ def getPunc (wordList):
     return puncList
 
 def makePuncCountDictionary (puncList):
-    # returns a dictionary of punctuation marks and their frequencies
+    """ returns a dictionary of punctuation marks and their frequencies """
     puncCountDictionary = {}
     for i in puncList:
         if not (i in list(puncCountDictionary.keys())):
@@ -143,8 +124,8 @@ def makePuncCountDictionary (puncList):
     
     return puncCountDictionary
 
-def getStems(wordList):
-    # returns a list of all word stems found
+def getStems (wordList):
+    """ returns a list of all word stems found """
     stemList = []
     for i in wordList:
         for j in suffixes:
@@ -157,7 +138,7 @@ def getStems(wordList):
     return stemList       
 
 def makeStemsDictionary (stemList):
-    # returns a dictionary of stems and their frequencies
+    """ returns a dictionary of stems and their frequencies """
     stemsDictionary = {}
     for i in stemList:
         if not (i in list(stemsDictionary.keys())):
@@ -168,18 +149,16 @@ def makeStemsDictionary (stemList):
     return stemsDictionary
 
 def normaliseDictionary (dictionary):
-    # returns a normalised version (value = between 0 and 1) of any input dictionary
+    """ returns a normalised version (value = between 0 and 1) of any input dictionary """
     counts = sum(list(dictionary.values()))
     for i in list(dictionary.keys()):
         dictionary[i] = dictionary[i]/counts
     
     return dictionary
 
-def buildTextModel(fileName):
-    """ Calls dictionary building functions to create 5 dictionaries for a given text file.
-        Returns the dictionaries in a list. 
-    """
-
+def createAllDictionaries (fileName):
+    """ calls dictionary building functions to create 5 dictionaries for a given text file.
+        returns the dictionaries in a list. """
     text = readTextFromFile(fileName) # a wordList
     sentences = getSentences(text) # a sentenceList
     puncs = getPunc(text) # a puncList
@@ -195,7 +174,7 @@ def buildTextModel(fileName):
     return [wordCount, wordLength, stemsDictionary, sentenceLength, punctuation]
 
 def normaliseModel (TM):
-    # normalises all dictionaries in textModel
+    """ normalises all dictionaries in textModel """
     outputTextModel = []
     for i in TM:
         outputTextModel.append(normaliseDictionary(i))
@@ -203,37 +182,64 @@ def normaliseModel (TM):
     return outputTextModel
 
 def smallestValue (dictionary1, dictionary2):
-    # returns the smallest value between both dictionary1 and dictionary2
+    """ returns the smallest value between both dictionary1 and dictionary2 """
     min1 = min(dictionary1.values())
     min2 = min(dictionary2.values())
     return min(min1, min2)
 
 def compareDictionaries (unknownDictionary, dictionary1, dictionary2):
+    """ """
     minvalue = smallestValue(dictionary1, dictionary2)
-    minvalue = 0.001
 
     probability1 = 1
+    probability2 = 1
+
     for i in unknownDictionary:
         if i in dictionary1.keys(): 
-            probability1 *= dictionary1[i]
+            probability1 *= dictionary1[i]**unknownDictionary[i]
         else:
             probability1 *= minvalue/2
     
-    probability2 = 1
-    for i in unknownDictionary:
         if i in dictionary2.keys(): 
-            probability2 *= dictionary2[i]
+            probability2 *= dictionary2[i]**unknownDictionary[i]
         else:
             probability2 *= minvalue/2
 
-    return [math.log(probability1), math.log(probability2)]
+    if probability1 == 0 and probability2 == 0:
+        return[0,0]
+    elif probability1 == 0:
+        return[0, math.log(probability2)]
+    elif probability2 == 0:
+        return[math.log(probability2), 0]
+    else:
+        return [math.log(probability1), math.log(probability2)]
+
+def compareAllDictionaries (textModel, normalModel1, normalModel2):
+    """ Returns a list including: [ model with best fit, # attributes that model1 was better at, # attributes that model2 was better at. 
+    Takes in a non-normalised text model and two normalised models. """
+    results = []
+    model1WinCount = 0
+    model2WinCount = 0
+    tieCount = 0
+    for i in range(len(textModel)):
+        comparison = compareDictionaries(textModel[i], normalModel1[i], normalModel2[i])
+        if comparison[0] > comparison[1]: 
+            results.append("Model 1.")
+            model1WinCount += 1
+        elif comparison[1] > comparison[0]: 
+            results.append("Model 2.")
+            model2WinCount += 1
+        else:
+            results.append("a tie.")
+            tieCount += 1
+
+    return [results, model1WinCount, model2WinCount]
 
 # TODO: change TM to textModel
 # a function to print all of the dictionaries in a TextModel1
-def printAllDictionaries( TM ):
+def printAllDictionaries (TM):
     """ a function to print all of the dictionaries in TM
-        input: TM, a text model (a list of 5 or more dictionaries)
-    """
+        input: TM, a text model (a list of 5 or more dictionaries) """
     words = TM[0]
     wordlengths = TM[1]
     stems = TM[2]
@@ -248,24 +254,30 @@ def printAllDictionaries( TM ):
     # print("\nNormalised Words:\n", TM[5])
     print("\n\n")
 
-# and, test things out here...
-TextModel1 = buildTextModel("testText1.txt") 
-print("TextModel1:")
-printAllDictionaries(TextModel1)
-normalModel1 = normaliseModel(TextModel1)
-printAllDictionaries(normalModel1)
 
-TextModel2 = buildTextModel("testText2.txt") 
-print("TextModel2:")
-printAllDictionaries(TextModel2)
-normalModel2 = normaliseModel(TextModel2)
-printAllDictionaries(normalModel2)
+def compareTexts (fileName1, fileName2, fileName3):
+    """ Model 1 is the one used to compare against Model 2 and Model 3.
+    Main function for algorithm. """
+    TextModel1 = createAllDictionaries(fileName1) 
 
-TextModel3 = buildTextModel("testText3.txt") 
-print("TextModel3:")
-printAllDictionaries(TextModel3)
-normalModel3 = normaliseModel(TextModel3)
-printAllDictionaries(normalModel3)
+    TextModel2 = createAllDictionaries(fileName2) 
+    normalModel2 = normaliseModel(TextModel2)
 
-minValue = smallestValue(normalModel2[0], normalModel3[0])
-compareDictionaries(normalModel1[0], normalModel2[0], normalModel3[0])
+    TextModel3 = createAllDictionaries(fileName3) 
+    normalModel3 = normaliseModel(TextModel3)
+
+    results = compareAllDictionaries(TextModel1, normalModel2, normalModel3)
+    print("The better model for words was", results[0][0])
+    print("The better model for word lengths was", results[0][1])
+    print("The better model for stems was", results[0][2])
+    print("The better model for sentence lengths was", results[0][3])
+    print("The better model for punctuation was", results[0][4])
+    
+    if results[1] > results[2]: 
+        print("Model 1 was a better model overall.")
+    elif results[1] < results[2]: 
+        print("Model 2 was a better model overall.")
+    else:
+        print("Model 1 ties with Model 2.")
+
+compareTexts("rowling1.txt", "rowling2.txt", "shakespeare.txt")
